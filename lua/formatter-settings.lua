71b64fa694486d818e40374deb7c8a4536dc939f
Will Adams <bushblade@gmail.com>
2021-07-15 12:23:55 +0100

    formatting

---
+++ lua/formatter-settings.lua
@@ -0,0 +1,44 @@
+
+-- Prettier function for formatter
+local prettier = function()
+  return {
+    exe = "prettier",
+    args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+    stdin = true
+  }
+end
+
+require("formatter").setup(
+  {
+    logging = false,
+    filetype = {
+      javascript = {prettier},
+      typescript = {prettier},
+      html = {prettier},
+      css = {prettier},
+      scss = {prettier},
+      markdown = {prettier},
+      lua = {
+        -- luafmt
+        function()
+          return {
+            exe = "luafmt",
+            args = {"--indent-count", 2, "--stdin"},
+            stdin = true
+          }
+        end
+      }
+    }
+  }
+)
+
+-- Runs Prettier on save
+vim.api.nvim_exec(
+  [[
+augroup FormatAutogroup
+  autocmd!
+  autocmd BufWritePost *.js,*.ts,*.css,*.scss,*.md,*.html,*.lua : FormatWrite
+augroup END
+]],
+  true
+)
