43fafd149110b20fb5905af929dcffc3af683e0d
Will Adams <bushblade@gmail.com>
2021-07-18 08:48:00 +0100

    themeing

--- lua/options.lua
+++ lua/options.lua
@@ -31,6 +31,11 @@ vim.g.closetag_filenames = "*.html,*.xhtml,*.phtml,*.php,*.jsx,*.js,*.tsx,*.vue"
 
 -- set colour scheme
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 vim.cmd [[colorscheme tokyonight]]
 
 -- highlight on yank
