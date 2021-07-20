315517d68abd1f19ddb9bc3e9d79706305b9d48b
Will Adams <bushblade@gmail.com>
2021-07-20 20:52:49 +0100

    go to definition working in JS

--- lua/lsp-settings.lua
+++ lua/lsp-settings.lua
@@ -25,6 +25,20 @@ nvim_lsp.tsserver.setup {
   end,
   -- nvim-lsp-ts-utils settings below
   on_attach = function(client, bufnr)
+    local opts = {noremap = true, silent = true}
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+      vim.api.nvim_buf_set_option(bufnr, ...)
+    end
+
+    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+
     -- disable tsserver formatting if you plan on formatting via null-ls
     client.resolved_capabilities.document_formatting = false
 
