6c54b28192478057be25772fc6e92a963cd696f9
Will Adams <bushblade@gmail.com>
2021-07-21 14:19:09 +0100

    plugin handles formmatting

--- lua/language-servers/json.lua
+++ lua/language-servers/json.lua
@@ -2,7 +2,7 @@ require "lspconfig".jsonls.setup {
   cmd = {"vscode-json-language-server", "--stdio"},
   filetypes = {"json"},
   init_options = {
-    provideFormatter = true
+    provideFormatter = false
   },
   root_dir = function()
     return vim.loop.cwd()
