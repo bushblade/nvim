524696fb5898b04eafcefaac6de6eb989a284f7c
Will Adams <bushblade@gmail.com>
2021-07-22 11:02:39 +0100

    sizing

--- lua/telescope-settings.lua
+++ lua/telescope-settings.lua
@@ -18,7 +18,7 @@ require("telescope").setup {
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
     layout_config = {
-      preview_width = 70,
+      preview_width = 0.6,
       horizontal = {
         mirror = false
       },
