3182fb533aead33fec2cd14e6754c7ab8a82d886
Will Adams <bushblade@gmail.com>
2021-08-18 09:23:02 +0100

    theming

--- lua/theme-settings.lua
+++ lua/theme-settings.lua
@@ -1,3 +1,6 @@
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = {hint = "orange", error = "#ff0000"}
+vim.cmd [[colorscheme tokyonight]]
 -- set colour scheme
 vim.g.tokyonight_style = "night"
 -- vim.g.tokyonight_sidebars = {"qf", "vista_kind", "terminal", "packer"}
