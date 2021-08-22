c22e36af65572b12da9177a7fd71d2666f11e1fb
Will Adams <bushblade@gmail.com>
2021-08-22 20:42:20 +0100

    Prefer default

--- lua/telescope-settings.lua
+++ lua/telescope-settings.lua
@@ -12,8 +12,7 @@ telescope.setup {
       "--column",
       "--smart-case"
     },
-    -- prompt_prefix = "> ",
-    prompt_prefix = "🔭 ",
+    prompt_prefix = "> ",
     selection_caret = "> ",
     entry_prefix = "  ",
     initial_mode = "insert",
