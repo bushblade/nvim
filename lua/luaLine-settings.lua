2a192d9ba34c8341e4f173daae49fe7c83a87f03
Will Adams <bushblade@gmail.com>
2021-08-22 20:28:49 +0100

    remove filetype

--- lua/luaLine-settings.lua
+++ lua/luaLine-settings.lua
@@ -11,7 +11,8 @@ require "lualine".setup {
     lualine_a = {"mode"},
     lualine_b = {"branch"},
     lualine_c = {"filename"},
-    lualine_x = {"encoding", "fileformat", "filetype"},
+    -- lualine_x = {"encoding", "fileformat", "filetype"},
+    lualine_x = {"encoding", "filetype"},
     lualine_y = {"progress"},
     lualine_z = {"location"}
   },
