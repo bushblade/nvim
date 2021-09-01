f906579d7ef110a4b9f5d348ebb839d16d0bca3c
Will Adams <bushblade@gmail.com>
2021-09-01 12:27:56 +0100

    only ingore unknowAt in tailwind project

--- lua/ls/css.lua
+++ lua/ls/css.lua
@@ -1,18 +1,23 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+local css_settings = {
+  validate = true,
+  lint = {}
+}
+
+local is_tailwind = vim.fn.filereadable("tailwind.config.js")
+if is_tailwind == 1 then
+  css_settings.lint.unknownAtRules = "ignore"
+end
+
 require("lspconfig").cssls.setup(
   {
     capabilities = capabilities,
     cmd = {"vscode-css-language-server", "--stdio"},
     filetypes = {"css", "scss", "less"},
     settings = {
-      css = {
-        validate = true,
-        lint = {
-          unknownAtRules = "ignore"
-        }
-      },
+      css = css_settings,
       less = {
         validate = true
       },
