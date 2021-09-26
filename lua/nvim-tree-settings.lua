521c518143c8134e18e14c7b2f6633640337b4c2
Will Adams <bushblade@gmail.com>
2021-09-26 15:39:44 +0100

    move settings nvim-tree

--- lua/nvim-tree-settings.lua
+++ lua/nvim-tree-settings.lua
@@ -1,10 +1,8 @@
 vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "*.d.ts" }
-vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
-vim.g.nvim_tree_gitignore = 1
+-- vim.g.nvim_tree_gitignore = 1
 
 -- icons
 vim.g.nvim_tree_icons = {
@@ -18,4 +16,8 @@ vim.g.nvim_tree_icons = {
 
 require("nvim-tree").setup({
   auto_close = true,
+  lsp_diagnostics = true,
+  update_focused_file = {
+    enable = true,
+  },
 })
