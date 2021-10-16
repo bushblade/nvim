5c073701fba410deaa22e39941c31dd08259c215
Will Adams <bushblade@gmail.com>
2021-10-16 10:47:03 +0100

    notes

--- lua/treesitter-settings.lua
+++ lua/treesitter-settings.lua
@@ -6,7 +6,7 @@ require("nvim-treesitter.configs").setup({
   -- ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "html" }, -- disabled for now https://github.com/nvim-treesitter/nvim-treesitter/issues/1788
+    disable = { "html" }, -- FIX: disabled for now https://github.com/nvim-treesitter/nvim-treesitter/issues/1788
   },
   autotag = { enable = true },
 })
