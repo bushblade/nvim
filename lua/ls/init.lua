87394288aaf463f4757471b59466a4fbb08a434a
Will Adams <bushblade@gmail.com>
2021-10-30 09:28:10 +0100

    cleanup

--- lua/ls/init.lua
+++ lua/ls/init.lua
@@ -19,10 +19,10 @@ for type, icon in pairs(signs) do
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  -- virtual_text = true,
   virtual_text = {
     prefix = " ",
     spacing = 2,
+    source = "always", -- Or "if_many"
   },
   signs = true,
   underline = true,
