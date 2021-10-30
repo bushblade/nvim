eb7e3782496ef942d78cdada9d3278105f5f5064
Will Adams <bushblade@gmail.com>
2021-10-30 09:10:47 +0100

    update for 5.1

--- lua/ls/init.lua
+++ lua/ls/init.lua
@@ -18,43 +18,13 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- Show source in diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _)
-  local config = {
-    -- your config
-    -- see :h on_publish_diagnostics
-    underline = true,
-    virtual_text = {
-      prefix = " ",
-      spacing = 2,
-    },
-    signs = true,
-    update_in_insert = false,
-  }
-  local uri = params.uri
-  local bufnr = vim.uri_to_bufnr(uri)
-
-  if not bufnr then
-    return
-  end
-
-  local diagnostics = params.diagnostics
-
-  for i, v in ipairs(diagnostics) do
-    diagnostics[i].message = string.format("%s: %s", v.source, v.message)
-  end
-
-  vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
-
-  if not vim.api.nvim_buf_is_loaded(bufnr) then
-    return
-  end
-
-  vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
-end
-
--- Show diagnostics automatically in hover window
--- You will likely want to reduce updatetime which affects CursorHold
--- note: this setting is global and should be set only once
--- vim.o.updatetime = 250
--- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  -- virtual_text = true,
+  virtual_text = {
+    prefix = " ",
+    spacing = 2,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
