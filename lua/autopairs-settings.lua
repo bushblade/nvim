5341b34d7f9c40cb0a4969b4ef0f2c7c324321e3
Will Adams <bushblade@gmail.com>
2021-10-31 15:28:42 +0000

    change to autopairs settings

--- lua/autopairs-settings.lua
+++ lua/autopairs-settings.lua
@@ -1,8 +1,6 @@
 require("nvim-autopairs").setup({})
 
--- you need setup cmp first put this after cmp.setup()
-require("nvim-autopairs.completion.cmp").setup({
-	map_cr = true, --  map <CR> on insert mode
-	map_complete = true, -- it will auto insert `(` after select function or method item
-	auto_select = true, -- automatically select the first item
-})
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
