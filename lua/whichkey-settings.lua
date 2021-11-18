1699d987557f01796445b898d81ea6a024a91e38
Will Adams <bushblade@gmail.com>
2021-11-18 11:24:23 +0000

    theming

--- lua/whichkey-settings.lua
+++ lua/whichkey-settings.lua
@@ -34,7 +34,7 @@ wk.setup({
     position = "bottom", -- bottom, top
     margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
     padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-    winblend = 10,
+    -- winblend = 0,
   },
   layout = {
     height = { min = 4, max = 25 }, -- min and max height of the columns
