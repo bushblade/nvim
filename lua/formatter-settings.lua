b037d7270b79383eca3e082fa85b4a6b05d51344
Will Adams <bushblade@gmail.com>
2021-11-23 19:44:42 +0000

    fix: no file found on file path prefixed $

--- lua/formatter-settings.lua
+++ lua/formatter-settings.lua
@@ -2,7 +2,7 @@
 local prettier = function()
   return {
     exe = "prettier",
-    args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+    args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
     stdin = true,
   }
 end
