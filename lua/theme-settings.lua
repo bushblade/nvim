1ea31049524fb45c394f8d8a88a89487630be1f2
Will Adams <bushblade@gmail.com>
2021-12-03 13:01:02 +0000

    transparent float

--- lua/theme-settings.lua
+++ lua/theme-settings.lua
@@ -1,10 +1,6 @@
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.cmd([[colorscheme tokyonight]])
--- set colour scheme
 vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_sidebars = {"qf", "vista_kind", "terminal", "packer"}
 vim.g.tokyonight_transparent = true
-vim.g.tokyonight_dark_sidebar = false
 vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_dark_float = false
+vim.cmd([[colorscheme tokyonight]])
+
