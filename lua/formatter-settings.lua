ecc21c26eceb8d3ff7cb0f0fc213abde43c110bb
Will Adams <bushblade@gmail.com>
2021-12-05 18:28:59 +0000

    remove args

--- lua/formatter-settings.lua
+++ lua/formatter-settings.lua
@@ -2,7 +2,7 @@
 local prettier = function()
   return {
     exe = "prettier",
-    args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote" },
+    args = { "--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)) },
     stdin = true,
   }
 end
