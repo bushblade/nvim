4e355338c8ad7720fea1a9586e4c6b7c7686bd6d
Will Adams <bushblade@gmail.com>
2021-12-24 16:04:35 +0000

    FIX: diagnostic signs not showing correctly

--- lua/ls/init.lua
+++ lua/ls/init.lua
@@ -17,8 +17,8 @@ require("ls.emmet")
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
