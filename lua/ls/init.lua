2c96f67b3b89acae8da905c3c449de95d16705b7
Will Adams <bushblade@gmail.com>
2022-01-05 21:38:22 +0000

    theming and border on floating windows

--- lua/ls/init.lua
+++ lua/ls/init.lua
@@ -30,3 +30,19 @@ vim.diagnostic.config({
     source = "always",
   },
 })
+
+vim.lsp.handlers["textDocument/hover"] =
+  vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = "single"
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+  vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = "single"
+  }
+)
