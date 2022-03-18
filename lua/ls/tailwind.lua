2be9afe9a6c19b4b680b8cc9d2f5dacf896ac2e8
Will Adams <bushblade@gmail.com>
2022-03-18 16:59:47 +0000

    only run tailwind ls in tailwind project

--- lua/ls/tailwind.lua
+++ lua/ls/tailwind.lua
@@ -1,1 +1,5 @@
-require("lspconfig").tailwindcss.setup({})
+local root_pattern = require("lspconfig").util.root_pattern
+
+require("lspconfig").tailwindcss.setup({
+  root_dir = root_pattern("tailwind.config.js", "tailwind.config.ts", "postcss.config.js", "postcss.config.ts"),
+})
