ea31b450932f8f08b3cbbee03346aebedcdaaf2d
Will Adams <bushblade@gmail.com>
2022-04-11 21:20:04 +0100

    remove deprecated setting

--- lua/treesitter-settings.lua
+++ lua/treesitter-settings.lua
@@ -2,7 +2,7 @@ require("nvim-treesitter.configs").setup({
   context_commentstring = {
     enable = true,
   },
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   -- ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
