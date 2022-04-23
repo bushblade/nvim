396baacc0283c10a057ebf6229a3b8dd74521b86
Will Adams <bushblade@gmail.com>
2022-04-23 14:54:51 +0100

    use new keymap.set function

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,86 +1,102 @@
 local function register_mappings(mappings, default_options)
-	for mode, mode_mappings in pairs(mappings) do
-		for _, mapping in pairs(mode_mappings) do
-			local options = #mapping == 3 and table.remove(mapping) or default_options
-			local prefix, cmd = unpack(mapping)
-			pcall(vim.api.nvim_set_keymap, mode, prefix, cmd, options)
-		end
-	end
+  for mode, mode_mappings in pairs(mappings) do
+    for _, mapping in pairs(mode_mappings) do
+      local options = #mapping == 3 and table.remove(mapping) or default_options
+      local prefix, cmd = unpack(mapping)
+      pcall(vim.keymap.set, mode, prefix, cmd, options)
+    end
+  end
 end
+
+local function telescope_find_files()
+  require("telescope.builtin").find_files({ hidden = true })
+end
+
+local border_options = { float = { border = "rounded" } }
 
 -- NOTE: <leader> prefixed mappings are in whichkey-settings.lua
 
 local mappings = {
-	i = {
-		-- Insert mode
-		{ "kk", "<ESC>" },
-		{ "jj", "<ESC>" },
-		{ "jk", "<ESC>" },
-		-- Terminal window navigation
-		{ "<C-h>", "<C-\\><C-N><C-w>h" },
-		{ "<C-j>", "<C-\\><C-N><C-w>j" },
-		{ "<C-k>", "<C-\\><C-N><C-w>k" },
-		{ "<C-l>", "<C-\\><C-N><C-w>l" },
-		-- moving text
-		{ "<C-j>", "<esc>:m .+1<CR>==" },
-		{ "<C-k>", "<esc>:m .-2<CR>==" },
-	},
-	n = {
-		-- Normal mode
-		-- Better window movement
-		{ "<C-h>", "<C-w>h", { silent = true } },
-		{ "<C-j>", "<C-w>j", { silent = true } },
-		{ "<C-k>", "<C-w>k", { silent = true } },
-		{ "<C-l>", "<C-w>l", { silent = true } },
-		-- Resize with arrows
-		{ "<C-Up>", ":resize -2<CR>", { silent = true } },
-		{ "<C-Down>", ":resize +2<CR>", { silent = true } },
-		{ "<C-Left>", ":vertical resize -2<CR>", { silent = true } },
-		{ "<C-Right>", ":vertical resize +2<CR>", { silent = true } },
-		-- Ctrl + p fuzzy files
-		{ "<C-p>", [[<cmd> lua require"telescope.builtin".find_files({ hidden = true })<CR>]] },
-		-- escape clears highlighting
-		{ "<esc>", ":noh<cr><esc>" },
-		-- hop words
-		{ "f", ":HopWord<cr>" },
-		{ "F", ":HopLine<cr>" },
-		-- yank to end of line on Y
-		{ "Y", "y$" },
-		-- lsp mappings
-		{ "K", "<Cmd>lua vim.lsp.buf.hover()<CR>" },
-		{ "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>" },
-		{ "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({ float = { border = 'rounded' }})<CR>" },
-		{ "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({ float = { border = 'rounded' }})<CR>" },
-		{ "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>" },
-		{ "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>" },
-		{ "gr", "<Cmd>lua vim.lsp.buf.references()<CR>" },
-		-- bufferline
-		{ "H", ":BufferLineCyclePrev<CR>" },
-		{ "L", ":BufferLineCycleNext<CR>" },
-	},
-	t = {
-		-- Terminal mode
-		-- Terminal window navigation
-		{ "<C-h>", "<C-\\><C-N><C-w>h" },
-		{ "<C-j>", "<C-\\><C-N><C-w>j" },
-		{ "<C-k>", "<C-\\><C-N><C-w>k" },
-		{ "<C-l>", "<C-\\><C-N><C-w>l" },
-		-- map escape to normal mode in terminal
-		{ "<Esc>", [[ <C-\><C-n> ]] },
-		{ "jj", [[ <C-\><C-n> ]] },
-	},
-	v = {
-		-- Visual/Select mode
-		-- Better indenting
-		{ "<", "<gv" },
-		{ ">", ">gv" },
-		-- hop words
-		{ "f", "<cmd>lua require'hop'.hint_words()<cr>" },
-		-- moving text
-		{ "J", ":m '>+1<CR>gv=gv" },
-		{ "K", ":m '<-2<CR>gv=gv" },
-	},
-	x = {},
+  i = {
+    -- Insert mode
+    { "kk", "<ESC>" },
+    { "jj", "<ESC>" },
+    { "jk", "<ESC>" },
+    -- Terminal window navigation
+    { "<C-h>", "<C-\\><C-N><C-w>h" },
+    { "<C-j>", "<C-\\><C-N><C-w>j" },
+    { "<C-k>", "<C-\\><C-N><C-w>k" },
+    { "<C-l>", "<C-\\><C-N><C-w>l" },
+    -- moving text
+    { "<C-j>", "<esc>:m .+1<CR>==" },
+    { "<C-k>", "<esc>:m .-2<CR>==" },
+  },
+  n = {
+    -- Normal mode
+    -- Better window movement
+    { "<C-h>", "<C-w>h", { silent = true } },
+    { "<C-j>", "<C-w>j", { silent = true } },
+    { "<C-k>", "<C-w>k", { silent = true } },
+    { "<C-l>", "<C-w>l", { silent = true } },
+    -- Resize with arrows
+    { "<C-Up>", ":resize -2<CR>", { silent = true } },
+    { "<C-Down>", ":resize +2<CR>", { silent = true } },
+    { "<C-Left>", ":vertical resize -2<CR>", { silent = true } },
+    { "<C-Right>", ":vertical resize +2<CR>", { silent = true } },
+    -- Ctrl + p fuzzy files
+    { "<C-p>", telescope_find_files },
+    -- escape clears highlighting
+    { "<esc>", ":noh<cr><esc>" },
+    -- hop words
+    { "f", ":HopWord<cr>" },
+    { "F", ":HopLine<cr>" },
+    -- yank to end of line on Y
+    { "Y", "y$" },
+    -- lsp mappings
+    { "K", vim.lsp.buf.hover },
+    { "<C-k>", vim.lsp.buf.signature_help },
+    {
+      "[d",
+      function()
+        vim.lsp.diagnostic.goto_prev(border_options)
+      end,
+    },
+    {
+      "]d",
+      function()
+        vim.lsp.diagnostic.goto_next(border_options)
+      end,
+    },
+    { "gD", vim.lsp.buf.declaration },
+    { "gd", vim.lsp.buf.definition },
+    { "gr", vim.lsp.buf.references },
+    -- bufferline
+    { "H", ":BufferLineCyclePrev<CR>" },
+    { "L", ":BufferLineCycleNext<CR>" },
+  },
+  t = {
+    -- Terminal mode
+    -- Terminal window navigation
+    { "<C-h>", "<C-\\><C-N><C-w>h" },
+    { "<C-j>", "<C-\\><C-N><C-w>j" },
+    { "<C-k>", "<C-\\><C-N><C-w>k" },
+    { "<C-l>", "<C-\\><C-N><C-w>l" },
+    -- map escape to normal mode in terminal
+    { "<Esc>", [[ <C-\><C-n> ]] },
+    { "jj", [[ <C-\><C-n> ]] },
+  },
+  v = {
+    -- Visual/Select mode
+    -- Better indenting
+    { "<", "<gv" },
+    { ">", ">gv" },
+    -- hop words
+    { "f", require("hop").hint_words },
+    -- moving text
+    { "J", ":m '>+1<CR>gv=gv" },
+    { "K", ":m '<-2<CR>gv=gv" },
+  },
+  x = {},
 }
 
 register_mappings(mappings, { silent = true, noremap = true })
@@ -91,5 +107,5 @@ vim.cmd("nnoremap S :%s/")
 -- hop in motion
 local actions = { "d", "c", "<", ">", "y" }
 for _, a in ipairs(actions) do
-	vim.api.nvim_set_keymap("n", a .. "f", a .. "<cmd>lua require'hop'.hint_char1()<cr>", {})
+  vim.keymap.set("n", a .. "f", a .. "<cmd>lua require'hop'.hint_char1()<cr>")
 end
