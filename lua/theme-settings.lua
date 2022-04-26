62fca7fd0845c9c8b7dd4c8513ba8fef4abd1b1c
Will Adams <bushblade@gmail.com>
2022-04-26 08:55:58 +0100

    transparent varient

--- lua/theme-settings.lua
+++ lua/theme-settings.lua
@@ -1,5 +1,5 @@
 vim.g.tokyonight_style = "night"
--- vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent = true
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_dark_float = false
 vim.cmd([[colorscheme tokyonight]])
