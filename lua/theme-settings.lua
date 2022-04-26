88690fc6ea22e575c9ccb39c73336b6e52e41e0b
Will Adams <bushblade@gmail.com>
2022-04-26 15:55:30 +0100

    nicer split separator

--- lua/theme-settings.lua
+++ lua/theme-settings.lua
@@ -2,4 +2,10 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_dark_float = false
+-- vim.g.tokyonight_hide_inactive_statusline = true
 vim.cmd([[colorscheme tokyonight]])
+
+local colors = require("tokyonight.colors").setup({})
+
+-- vim.cmd("highlight WinSeparator guifg=" .. colors.blue)
+vim.cmd("highlight WinSeparator guifg=" .. colors.bg_highlight)
