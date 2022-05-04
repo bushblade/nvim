a5562b73ae06dfe87868df22cac5d97e86edea2b
Will Adams <bushblade@gmail.com>
2022-05-04 16:37:54 +0100

    icon

--- lua/dashboard-settings.lua
+++ lua/dashboard-settings.lua
@@ -19,7 +19,7 @@ dashboard.section.buttons.val = {
   dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
   dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
   -- dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-  dashboard.button("s", "  > Sessions", ":Telescope session-lens search_session<CR>"),
+  dashboard.button("s", "  > Sessions", ":Telescope session-lens search_session<CR>"),
   dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
 
