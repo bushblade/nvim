8118c70217bf0d9d87bdbbf168eb509f3b508d98
Will Adams <bushblade@gmail.com>
2022-05-04 16:05:54 +0100

    clean out comments

--- lua/dashboard-settings.lua
+++ lua/dashboard-settings.lua
@@ -23,22 +23,6 @@ dashboard.section.buttons.val = {
   dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
 }
 
--- Set footer
---   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
---   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
---   ```init.lua
---   return require('packer').startup(function()
---       use 'wbthomason/packer.nvim'
---       use {
---           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
---           requires = {'BlakeJC94/alpha-nvim-fortune'},
---           config = function() require("config.alpha") end
---       }
---   end)
---   ```
--- local fortune = require("alpha.fortune")
--- dashboard.section.footer.val = fortune()
-
 -- Send config to alpha
 alpha.setup(dashboard.opts)
 
