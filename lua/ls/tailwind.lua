1e5cd87a3c9359596e3af616b472bfda726fe175
Will Adams <bushblade@gmail.com>
2022-05-05 19:18:42 +0100

    launch tailwind ls if cjs config file present

--- lua/ls/tailwind.lua
+++ lua/ls/tailwind.lua
@@ -1,5 +1,11 @@
 local root_pattern = require("lspconfig").util.root_pattern
 
 require("lspconfig").tailwindcss.setup({
-  root_dir = root_pattern("tailwind.config.js", "tailwind.config.ts", "postcss.config.js", "postcss.config.ts"),
+  root_dir = root_pattern(
+    "tailwind.config.js",
+    "tailwind.config.ts",
+    "postcss.config.js",
+    "postcss.config.ts",
+    "tailwind.config.cjs"
+  ),
 })
