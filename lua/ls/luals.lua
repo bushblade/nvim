650b4d3b61a6e04474db3397c078fbee316798c3
Will Adams <bushblade@gmail.com>
2022-05-26 10:46:19 +0100

    Update lua ls with latest config closes #8

--- lua/ls/luals.lua
+++ lua/ls/luals.lua
@@ -1,21 +1,11 @@
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
--- By default, lua-language-server doesn't have a cmd set. This is because nvim-lspconfig does not
--- make assumptions about your path. You must add the following to your init.vim or init.lua to set
--- cmd to the absolute path ($HOME and ~ are not expanded) of your unzipped and compiled lua-language-server.
 -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#sumneko_lua
 
 require("lspconfig").sumneko_lua.setup({
-  cmd = { "/usr/bin/lua-language-server", "-E", "/usr/bin/lua-language-server/main.lua" },
   settings = {
     Lua = {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = "LuaJIT",
-        -- Setup your lua path
-        path = runtime_path,
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
