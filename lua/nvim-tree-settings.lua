f757d81f3ed7e44024a6d6ed95c991b274406f0f
Will Adams <bushblade@gmail.com>
2022-05-30 19:28:16 +0100

    close tree on file open

--- lua/nvim-tree-settings.lua
+++ lua/nvim-tree-settings.lua
@@ -1,31 +1,33 @@
 require("nvim-tree").setup({
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = " ",
-      info = " ",
-      warning = " ",
-      error = " ",
-    },
-  },
-  update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-  },
-  -- filters = {
-  --   custom = { ".git" },
-  -- },
-  renderer = {
-    highlight_git = true,
-    highlight_opened_files = "all",
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
-  },
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = " ",
+			info = " ",
+			warning = " ",
+			error = " ",
+		},
+	},
+	update_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	renderer = {
+		highlight_git = true,
+		highlight_opened_files = "all",
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
+	},
 })
