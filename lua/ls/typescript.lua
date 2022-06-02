411a244714269bf87a15b17c938fd19a36509d7c
Will Adams <bushblade@gmail.com>
2022-06-02 11:10:34 +0100

    tsserver wasn't running from configuring deno-ls

--- lua/ls/typescript.lua
+++ lua/ls/typescript.lua
@@ -11,12 +11,5 @@ require("lspconfig").tsserver.setup({
     "typescript.tsx",
   },
   cmd = { "typescript-language-server", "--stdio" },
-  root_dir = function()
-    if root_pattern("deno.json", "deno.jsonc, denon.json") then
-      return false
-    else
-      -- I needed this to work on plain .js files
-      return vim.loop.cwd()
-    end
-  end,
+  root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", "index.js", "app.js"),
 })
