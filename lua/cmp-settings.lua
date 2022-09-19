0d9f3516bcc0e164d350717c1216354f5eebbecc
Will Adams <bushblade@gmail.com>
2022-09-19 20:56:32 +0100

    more cmdline completions

--- lua/cmp-settings.lua
+++ lua/cmp-settings.lua
@@ -98,6 +98,7 @@ cmp.setup.cmdline("/", {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = "path" },
   }, {
