4d40bfda25b7b399907ffb77b7affc69ded0a41a
Will Adams <bushblade@gmail.com>
2022-09-19 20:52:50 +0100

    cmp cmdline tab and <Shift> tab

--- lua/cmp-settings.lua
+++ lua/cmp-settings.lua
@@ -90,6 +90,7 @@ vim.cmd(
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
