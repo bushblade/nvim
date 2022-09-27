b1613ca57e5921c69a9a08dc1b95d0e36eff22d3
Will Adams <bushblade@gmail.com>
2022-09-27 17:59:00 +0100

    sort diagnostic severity

--- lua/ls/init.lua
+++ lua/ls/init.lua
@@ -22,24 +22,25 @@ require("ls.astro")
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 vim.diagnostic.config({
-	virtual_text = {
-		source = "if_many",
-		prefix = " ", -- Could be '●', '▎', 'x'
-	},
-	float = {
-		source = "always",
-	},
+  virtual_text = {
+    source = "if_many",
+    prefix = " ", -- Could be '●', '▎', 'x'
+  },
+  float = {
+    source = "always",
+  },
+  severity_sort = true,
 })
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded",
+  border = "rounded",
 })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "rounded",
+  border = "rounded",
 })
