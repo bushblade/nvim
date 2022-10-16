71b35198eec0f809f59aea523668f1f4dfbe0776
Will Adams <bushblade@gmail.com>
2022-10-16 17:39:40 +0100

    FIX: astro-ls needs path to global typescript

--- lua/ls/astro.lua
+++ lua/ls/astro.lua
@@ -1,1 +1,8 @@
-require("lspconfig").astro.setup({})
+require("lspconfig").astro.setup({
+  init_options = {
+    typescript = {
+      -- needs to be installed in ~ by using `pnpm i typescript`. NOTE: do not use -g flag!
+      serverPath = os.getenv("HOME") .. "/.npm-packages/lib/node_modules/typescript/lib/typescript.js",
+    },
+  },
+})
