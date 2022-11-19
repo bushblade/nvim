529442aa3b8e0a7f0d84259f28ef2758707ec163
Will Adams <bushblade@gmail.com>
2022-11-19 15:41:36 +0000

    treestitter seems to be working better in html now

--- lua/treesitter-settings.lua
+++ lua/treesitter-settings.lua
@@ -1,17 +1,17 @@
 require("nvim-treesitter.configs").setup({
-	context_commentstring = {
-		enable = true,
-	},
-	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	-- ignore_install = {}, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "html" }, -- FIX: disabled for now https://github.com/nvim-treesitter/nvim-treesitter/issues/1788
-	},
-	autotag = { enable = true },
-	indent = {
-		enable = true,
-	},
+  context_commentstring = {
+    enable = true,
+  },
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  -- ignore_install = {}, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    -- disable = { "html" }, -- FIX: disabled for now https://github.com/nvim-treesitter/nvim-treesitter/issues/1788
+  },
+  autotag = { enable = true },
+  indent = {
+    enable = true,
+  },
 })
 
 -- FIX: for nvim-autotag - not actually working
@@ -20,8 +20,8 @@ ts_utils.get_node_text = vim.treesitter.query.get_node_text
 
 -- Detect astro files and set filetype
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = { "*.astro" },
-	callback = function()
-		vim.cmd([[ set filetype=astro ]])
-	end,
+  pattern = { "*.astro" },
+  callback = function()
+    vim.cmd([[ set filetype=astro ]])
+  end,
 })
