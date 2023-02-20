e936c6775ab33c6a43afb23a4a6472db57875b3d
Will Adams <bushblade@gmail.com>
2023-02-20 22:44:44 +0000

    presence

--- lua/matchtag-settings.lua
+++
@@ -1,4 +0,0 @@
--- vim-matchtag
-vim.g.vim_matchtag_enable_by_default = 1
-vim.g.vim_matchtag_files = "*.astro,*.html,*.xml,*.js,*.jsx,*.vue,*.svelte,*.jsp,*.tsx"
-vim.g.vim_matchtag_highlight_cursor_on = 1
