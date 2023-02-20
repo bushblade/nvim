e83b6ac5e046d06dd4e43c9431b644a8d9edc91e
Will Adams <bushblade@gmail.com>
2023-02-20 22:21:26 +0000

    autopairs working

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,9 +2,16 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function()
+      require("nvim-autopairs").setup({})
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 }
+
+--
+-- -- If you want insert `(` after select function or method item
+-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+-- local cmp = require("cmp")
+-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
