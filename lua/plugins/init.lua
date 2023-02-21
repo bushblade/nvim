eb7db7ec75115acbd55cde2a7636923e6e0d8445
Will Adams <bushblade@gmail.com>
2023-02-21 11:32:24 +0000

    trouble and todos need to call setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,17 +1,29 @@
 return {
-  -- NOTE: First, some plugins that don't require any configuration
+  -- NOTE: plugins here require little to no configuratin
   "tpope/vim-fugitive",
   "tpope/vim-surround",
-  "knubie/vim-kitty-navigator",
+  -- NOTE: disabled for now as not working with nvim window bindings
+  -- "knubie/vim-kitty-navigator",
   "windwp/nvim-autopairs",
   "windwp/nvim-spectre", -- Spectre for find and replace
   "mhartington/formatter.nvim",
-  "mbbill/undotree",
   "delphinus/vim-firestore",
   "goolord/alpha-nvim",
   "andweeb/presence.nvim",
   "kyazdani42/nvim-web-devicons",
   { "numToStr/Comment.nvim", opts = {} },
   "rmagatti/auto-session",
-  -- "rmagatti/session-lens",
+  "airblade/vim-gitgutter",
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup()
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
 }
