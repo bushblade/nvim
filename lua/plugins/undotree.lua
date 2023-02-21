c3c240f68f5dc3e7717034b3605e3e1f568eb842
Will Adams <bushblade@gmail.com>
2023-02-21 11:32:02 +0000

    undo tree needs some config for persistance

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.cmd([[
+        if has("persistent_undo")
+           let target_path = expand('~/.undodir')
+            " create the directory and any parent directories
+            " if the location does not exist.
+            if !isdirectory(target_path)
+                call mkdir(target_path, "p", 0700)
+            endif
+            let &undodir=target_path
+            set undofile
+        endif
+        ]])
+    end,
+  },
+}
