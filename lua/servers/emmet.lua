6d4b8fdc0a2e618b7b28e63b95c3e4d45161dadc
Will Adams <bushblade@gmail.com>
2023-03-01 17:11:46 +0000

    emmet in astro files

--- lua/servers/emmet.lua
+++ lua/servers/emmet.lua
@@ -1,3 +1,3 @@
 require("lspconfig").emmet_ls.setup({
-  filetypes = { "html", "css", "javascriptreact", "typescriptreact", "vue" },
+  filetypes = { "html", "css", "javascriptreact", "typescriptreact", "vue", "astro" },
 })
