5a92f43bd9c2ac45f8f11512203e4913ed497540
Shivam <singhshivamkr@gmail.com>
2023-04-03 15:26:19 +0530

    feat: add mason for installing lsps

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+    config = function()
+      require("mason").setup()
+    end
+  }
+}
