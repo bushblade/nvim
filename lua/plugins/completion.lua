c5512c53fd913de67e053cfc829ac5059998244f
Will Adams <bushblade@gmail.com>
2023-04-10 18:21:46 +0100

    remove unused

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -61,7 +61,6 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
           ["<CR>"] = cmp.mapping.confirm({
-            -- behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           }),
           ["<Tab>"] = cmp.mapping(function(fallback)
