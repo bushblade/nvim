cf8bbcb7f7387f5d6ccfb31f6da4c74a200886e6
Will Adams <bushblade@gmail.com>
2023-05-24 13:42:47 +0100

    remove commmented out code

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -113,10 +113,6 @@ return {
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
-      -- FIX: for nvim-autotag - not actually working
-      -- local ts_utils = require("nvim-treesitter.ts_utils")
-      -- ts_utils.get_node_text = vim.treesitter.query.get_node_text
-
       -- Detect astro files and set filetype
       vim.api.nvim_create_autocmd("BufEnter", {
         pattern = { "*.astro" },
