ce488c297a860c447acc4c202427034039d2429b
Will Adams <bushblade@gmail.com>
2023-07-09 18:31:06 +0100

    transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,7 +13,7 @@ return {
           light = "latte",
           dark = "mocha",
         },
-        transparent_background = false, -- disables setting the background color.
+        transparent_background = true, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
