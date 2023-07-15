c3c293c40c0d150c86432a1536c3fc40ab602461
Will Adams <bushblade@gmail.com>
2023-07-15 09:02:01 +0100

    tab key for treesitter selections with flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -19,10 +19,16 @@ return {
       desc = "Flash",
     },
     {
-      "<CR>",
+      "<tab>",
       mode = { "n", "o", "x" },
       function()
-        require("flash").treesitter()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+
+        if filetype ~= "qf" then
+          -- only call flash.treesitter if not in a quickfix buffer
+          require("flash").treesitter()
+        end
       end,
       desc = "Flash Treesitter",
     },
