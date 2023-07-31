4da377dd79f6ee6be677068d8508e6ef9f34c69b
Will Adams <bushblade@gmail.com>
2023-07-31 12:24:55 +0100

    incremental selections now in flash

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -66,50 +66,6 @@ return {
       indent = {
         enable = true,
       },
-      -- incremental_selection = {
-      --   enable = true,
-      --   keymaps = {
-      --     init_selection = "<CR>",
-      --     node_incremental = "<TAB>",
-      --     scope_incremental = "<CR>",
-      --     node_decremental = "<S-TAB>",
-      --   },
-      -- },
-      -- textobjects = {
-      --   select = {
-      --     enable = true,
-      --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      --     keymaps = {
-      --       -- You can use the capture groups defined in textobjects.scm
-      --       ["aa"] = "@parameter.outer",
-      --       ["ia"] = "@parameter.inner",
-      --       ["af"] = "@function.outer",
-      --       ["if"] = "@function.inner",
-      --       ["ac"] = "@class.outer",
-      --       ["ic"] = "@class.inner",
-      --     },
-      --   },
-      --   move = {
-      --     enable = true,
-      --     set_jumps = true, -- whether to set jumps in the jumplist
-      --     goto_next_start = {
-      --       ["]m"] = "@function.outer",
-      --       ["]]"] = "@class.outer",
-      --     },
-      --     goto_next_end = {
-      --       ["]M"] = "@function.outer",
-      --       ["]["] = "@class.outer",
-      --     },
-      --     goto_previous_start = {
-      --       ["[m"] = "@function.outer",
-      --       ["[["] = "@class.outer",
-      --     },
-      --     goto_previous_end = {
-      --       ["[M"] = "@function.outer",
-      --       ["[]"] = "@class.outer",
-      --     },
-      --   },
-      -- },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
