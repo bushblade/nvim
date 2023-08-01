193a69793478878e3d1bbdeb187c38da4c4e2661
Will Adams <bushblade@gmail.com>
2023-08-01 14:08:19 +0100

    change mapping

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -60,7 +60,7 @@ return {
           ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
           ["<CR>"] = cmp.mapping.confirm({
