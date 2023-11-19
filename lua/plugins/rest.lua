446575589b762b6eb4348d8ea00c53c9363fb919
Will Adams <bushblade@gmail.com>
2023-11-19 14:55:46 +0000

    use latest rest.nvim - fixed formatting issue

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,8 +2,6 @@ return {
   {
     "rest-nvim/rest.nvim",
     requires = { "nvim-lua/plenary.nvim" },
-    -- latest commit breaks formatting
-    commit = "8b62563",
     opts = {
       result_split_in_place = true,
     },
