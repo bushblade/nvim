d1fcd63068418f708564fc7e3b7e7115a03ea937
Will Adams <bushblade@gmail.com>
2023-11-19 11:29:30 +0000

    specify commit for rest until they fix formatting

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,6 +2,8 @@ return {
   {
     "rest-nvim/rest.nvim",
     requires = { "nvim-lua/plenary.nvim" },
+    -- latest commit breaks formatting
+    commit = "8b62563",
     opts = {
       result_split_in_place = true,
     },
