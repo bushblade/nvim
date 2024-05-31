c7ef1efdf20c8ee3497ad4b3d2c70144902ddaf0
Will Adams <bushblade@gmail.com>
2024-05-31 16:16:34 +0100

    better function name

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -2,19 +2,19 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   opts = function()
-    local function use_biome()
+    local function is_biome()
       return vim.fn.glob("biome.json") ~= "" or vim.fn.glob("biome.jsonc") ~= ""
     end
 
     local formatters_by_ft = {
       lua = { "stylua" },
       python = { "autopep8" },
-      typescriptreact = use_biome() and { "biome" } or { "prettierd" },
-      javascriptreact = use_biome() and { "biome" } or { "prettierd" },
-      javascript = use_biome() and { "biome" } or { "prettierd" },
-      typescript = use_biome() and { "biome" } or { "prettierd" },
-      json = use_biome() and { "biome" } or { "prettierd" },
-      jsonc = use_biome() and { "biome" } or { "prettierd" },
+      typescriptreact = is_biome() and { "biome" } or { "prettierd" },
+      javascriptreact = is_biome() and { "biome" } or { "prettierd" },
+      javascript = is_biome() and { "biome" } or { "prettierd" },
+      typescript = is_biome() and { "biome" } or { "prettierd" },
+      json = is_biome() and { "biome" } or { "prettierd" },
+      jsonc = is_biome() and { "biome" } or { "prettierd" },
       html = { "prettierd" },
       css = { "prettierd" },
       scss = { "prettierd" },
