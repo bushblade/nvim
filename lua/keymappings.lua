1bc1aad35445da2c6a82d1d9a03c99417be4b246
Will Adams <bushblade@gmail.com>
2024-07-16 11:57:21 +0100

    move keymappings out of which key and remove deprecated methods from which key

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -26,9 +26,9 @@ map("n", "]d", function()
   vim.diagnostic.goto_next(border_options)
 end, { desc = "Next diagnostic" })
 map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
-map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Defenition" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
 map("n", "gr", vim.lsp.buf.references, { desc = "List References" })
-map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implemtation" })
+map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("n", "k", 'v:count == 0 ? "gk" : "k"', { expr = true })
@@ -47,3 +47,85 @@ map("v", "K", "<cmd>m '<-2<CR>gv=gv")
 
 -- Visual block mode mappings
 map("x", "<leader>p", '"_dP')
+
+-- Leader key mappings
+local function qf_toggle()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_exists = true
+    end
+  end
+  if qf_exists == true then
+    vim.cmd("cclose")
+    return
+  end
+  if not vim.tbl_isempty(vim.fn.getqflist()) then
+    vim.cmd("copen")
+  end
+end
+
+map("n", "<leader>q", qf_toggle, { desc = "Toggle Quickfix list" })
+map("n", "<leader>M", "<cmd>Mason<CR>", { desc = "Mason" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "File Tree" })
+map("n", "<leader>u", "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", { desc = "Undotree" })
+map("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "Lazy" })
+map("n", "<leader>c", require("actions-preview").code_actions, { desc = "Code Actions" })
+map("n", "<leader>d", function()
+  vim.diagnostic.open_float({ border = "rounded" })
+end, { desc = "Line Diagnostics" })
+map("n", "<leader>w", "<cmd>WhichKey<CR>", { desc = "WhichKey" })
+map("n", "<leader>s", function()
+  vim.o.spell = not vim.o.spell
+end, { desc = "Toggle spell check" })
+map("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename" })
+
+-- Telescope mappings
+local telescope_builtin = require("telescope.builtin")
+local session_lens = require("session-lens")
+
+map("n", "<leader>ff", function()
+  telescope_builtin.find_files({ hidden = true })
+end, { desc = "Find File" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find Buffer" })
+map("n", "<leader>fn", "<cmd>TodoTelescope<cr>", { desc = "Find Notes" })
+map("n", "<leader>ft", "<cmd>Telescope builtin<cr>", { desc = "Telescope builtin" })
+map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Search In Files" })
+map("n", "<leader>fr", "<cmd>Telescope lsp_references<cr>", { desc = "Find References" })
+map("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "Document Diagnostics" })
+map("n", "<leader>fm", "<cmd>Telescope marks<CR>", { desc = "Marks" })
+map("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Key mappings" })
+map("n", "<leader>fM", "<cmd>Telescope man_pages<CR>", { desc = "Man pages" })
+map("n", "<leader>fa", session_lens.search_session, { desc = "Search Sessions" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Search help" })
+map("n", "<leader>fT", "<cmd>TodoTelescope<CR>", { desc = "Search Todos" })
+map("n", "<leader>fe", "<cmd>Telescope file_browser<CR>", { desc = "Browse Files" })
+map("n", "<leader>fg", "<cmd>Telescope git_status<CR>", { desc = "Git Status" })
+
+-- LSP mappings
+map("n", "<leader>lf", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format File" })
+map("n", "<leader>ld", function()
+  vim.diagnostic.open_float({ border = "rounded" })
+end, { desc = "Line Diagnostics" })
+map("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
+map("n", "<leader>lR", vim.lsp.buf.references, { desc = "List references" })
+map("n", "<leader>lc", require("actions-preview").code_actions, { desc = "Code actions" })
+
+-- Spectre mappings
+map("n", "<leader>Ss", require("spectre").open_visual, { desc = "Open Spectre" })
+map("n", "<leader>Sw", function()
+  require("spectre").open_visual({ select_word = true })
+end, { desc = "Search for word under cursor" })
+
+-- Auto Sessions mappings
+map("n", "<leader>aS", "<cmd>SaveSession<cr>", { desc = "Save session" })
+map("n", "<leader>as", "<cmd>SearchSession<cr>", { desc = "Search sessions" })
+map("n", "<leader>ad", "<cmd>SessionDelete<cr>", { desc = "Delete session" })
+map("n", "<leader>ar", "<cmd>SessionRestore<cr>", { desc = "Restore session" })
+
+-- Bufferline and other mappings
+map("n", "<leader>b", "", { desc = "Bufferline" })
+map("n", "<leader>t", "", { desc = "Trouble" })
+map("n", "<leader>h", "", { desc = "GitSigns" })
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,132 +1,14 @@
-local function qf_toggle()
-  local qf_exists = false
-  for _, win in pairs(vim.fn.getwininfo()) do
-    if win["quickfix"] == 1 then
-      qf_exists = true
-    end
-  end
-  if qf_exists == true then
-    vim.cmd("cclose")
-    return
-  end
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd("copen")
-  end
-end
-
 return {
   {
     "folke/which-key.nvim",
-    event = "VimEnter",
-    config = function()
-      local wk = require("which-key")
-      wk.setup({
-        window = {
-          border = "single", -- none, single, double, shadow
-          position = "bottom", -- bottom, top
-          margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
-          padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-          -- winblend = 0,
-        },
-        triggers_blacklist = {
-          -- list of mode / prefixes that should never be hooked by WhichKey
-          -- this is mostly relevant for key maps that start with a native binding
-          -- most people should not need to change this
-          i = { "j", "k" },
-          v = { "j", "k" },
-        },
-      })
-      -- register key bindings with <leader> prefix
-      wk.register({
-        -- R = { "<cmd>Rest run<CR>", "Run with Rest" },
-        q = { qf_toggle, "Toggle Quickfix list" },
-        M = { "<cmd>Mason<CR>", "Mason" },
-        f = {
-          -- mostly Telescope bindings
-          name = "Find with Telescope",
-          f = {
-            function()
-              require("telescope.builtin").find_files({ hidden = true })
-            end,
-            "Find File",
-          },
-          b = { "<cmd>Telescope buffers<cr>", "Find Buffer" },
-          n = { "<cmd>TodoTelescope<cr>", "Find Notes" },
-          t = { "<cmd>Telescope builtin<cr>", "Telescope builtin" },
-          s = { "<cmd>Telescope live_grep<cr>", "Search In Files" },
-          r = { "<cmd>Telescope lsp_references<cr>", "Find References" },
-          d = { "<cmd>Telescope diagnostics<cr>", "Document Diagnostics" },
-          m = { "<cmd>Telescope marks<CR>", "Marks" },
-          k = { "<cmd>Telescope keymaps<CR>", "Key mappings" },
-          M = { "<cmd>Telescope man_pages<CR>", "Man pages" },
-          a = { require("session-lens").search_session, "Search Sessions" },
-          h = { "<cmd>Telescope help_tags<CR>", "Search help" },
-          T = { "<cmd>TodoTelescope<CR>", "Search Todos" },
-          e = { "<cmd>Telescope file_browser<CR>", "Browse Files" },
-          g = { "<cmd>Telescope git_status<CR>", "Git Status" },
-        },
-        e = { "<cmd>NvimTreeToggle<CR>", "File Tree" },
-        u = { "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", "Undotree" },
-        L = { "<cmd>Lazy<CR>", "Lazy" },
-        c = { require("actions-preview").code_actions, "Code Actions" },
-        d = {
-          function()
-            vim.diagnostic.open_float({ border = "rounded" })
-          end,
-          "Line Diagnostics",
-        },
-        w = { "<cmd>WhichKey<CR>", "WhichKey" },
-        s = {
-          function()
-            vim.o.spell = not vim.o.spell
-          end,
-          "Toggle spell check",
-        },
-        r = { vim.lsp.buf.rename, "Rename" },
-        -- Bufferline
-        b = {
-          name = "Bufferline",
-        },
-        S = {
-          name = "Spectre - find and replace",
-          s = { require("spectre").open_visual, "Open Spectre" },
-          w = {
-            function()
-              require("spectre").open_visual({ select_word = true })
-            end,
-            "Search for word under cursor",
-          },
-        },
-        l = {
-          name = "LSP",
-          f = {
-            function()
-              vim.lsp.buf.format({ async = true })
-            end,
-            "Format File",
-          },
-          d = {
-            function()
-              vim.diagnostic.open_float({ border = "rounded" })
-            end,
-            "Line Diagnostics",
-          },
-          r = { vim.lsp.buf.rename, "Rename" },
-          R = { vim.lsp.buf.references, "List references" },
-          c = { require("actions-preview").code_actions, "Code actions" },
-        },
-        a = {
-          name = "Auto Sessions",
-          S = { "<cmd>SaveSession<cr>", "Save session" },
-          s = { "<cmd>SearchSession<cr>", "Search sessions" },
-          d = { "<cmd>SessionDelete<cr>", "Delete session" },
-          r = { "<cmd>SessionRestore<cr>", "Restore session" },
-        },
-        t = { name = "Trouble" },
-        h = { name = "GitSigns" },
-      }, {
-        prefix = "<leader>",
-      })
-    end,
+    event = "VeryLazy",
+    opts = {
+      win = {
+        -- don't allow the popup to overlap with the cursor
+        no_overlap = true,
+        border = "rounded",
+        padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+      },
+    },
   },
 }
