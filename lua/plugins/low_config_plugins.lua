3a9b14cf940bd54c93b5522181173f62cc50a457
Will Adams <bushblade@gmail.com>
2024-08-17 12:14:25 +0100

    very lazy for spectre

--- lua/plugins/low_config_plugins.lua
+++ lua/plugins/low_config_plugins.lua
@@ -13,12 +13,13 @@ return {
 
   -- NOTE: disabled for now as not working with nvim window bindings
   -- "knubie/vim-kitty-navigator",
-  "windwp/nvim-spectre", -- Spectre for find and replace
+  { "windwp/nvim-spectre", event = "VeryLazy" }, -- Spectre for find and replace
   "delphinus/vim-firestore",
   "kyazdani42/nvim-web-devicons",
   -- Better code action previews
   {
     "aznhe21/actions-preview.nvim",
+    event = "VeryLazy",
   },
   -- Useful status updates for LSP
   { "j-hui/fidget.nvim", opts = { window = { border = "rounded", blend = 0 } }, tag = "legacy" },
