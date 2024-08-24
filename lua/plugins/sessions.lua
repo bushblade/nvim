95cb23d9dc5dac5a7a80c48792cd45f5777f4ff6
Will Adams <bushblade@gmail.com>
2024-08-24 09:53:29 +0100

    silent restore session

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -3,7 +3,7 @@ return {
     "rmagatti/session-lens",
     dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
     config = function()
-      require("auto-session").setup({})
+      require("auto-session").setup({ silent_restore = false })
     end,
   },
 }
