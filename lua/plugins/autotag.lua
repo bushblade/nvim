f122a6a49f658d2f84b7dde756024be12925427c
Will Adams <bushblade@gmail.com>
2024-10-22 13:29:18 +0100

    use config function so not to use legacy setup options

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,9 +1,13 @@
 return {
   "windwp/nvim-ts-autotag",
-  opts = {
-    -- Defaults
-    enable_close = true, -- Auto close tags
-    enable_rename = true, -- Auto rename pairs of tags
-    enable_close_on_slash = false, -- Auto close on trailing </
-  },
+  config = function()
+    require("nvim-ts-autotag").setup({
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
+      },
+    })
+  end,
 }
