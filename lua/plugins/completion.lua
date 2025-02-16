4b2843af35f685ccef4b4d11c23be65d9e6663d9
Will Adams <bushblade@gmail.com>
2025-02-16 09:53:21 +0000

    LSP for WebDev set up for using blink.cmp for completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,141 +1,21 @@
 return {
-  {
-
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-cmdline", -- command line
-      "hrsh7th/cmp-buffer", -- buffer completions
-      "hrsh7th/cmp-nvim-lua", -- nvim config completions
-      "hrsh7th/cmp-nvim-lsp", -- lsp completions
-      "hrsh7th/cmp-path", -- file path completions
-      "saadparwaiz1/cmp_luasnip", -- snippets completions
-      "hrsh7th/cmp-emoji",
-      {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
-      },
-      "onsails/lspkind-nvim",
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true }, -- tailwind color in suggesions
+  "saghen/blink.cmp",
+  dependencies = "rafamadriz/friendly-snippets",
+  version = "*",
+  opts = {
+    completion = {
+      menu = { border = "rounded" },
+      documentation = { window = { border = "rounded" } },
     },
-    event = "InsertEnter",
-    config = function()
-      local cmp = require("cmp")
-
-      local lspkind = require("lspkind")
-      local luasnip = require("luasnip")
-
-      -- html snippets in javascript and javascriptreact
-      -- luasnip.filetype_extend("javascriptreact", { "html", "jsdoc", "react-es7" })
-      -- luasnip.filetype_extend("typescriptreact", { "html", "tsdoc", "react-ts" })
-
-      luasnip.filetype_extend("javascript", { "jsdoc", "javascriptreact" })
-      luasnip.filetype_extend("typescript", { "tsdoc" })
-      luasnip.filetype_extend("typescriptreact", { "javascriptreact" })
-
-      local loader = require("luasnip.loaders.from_vscode")
-      loader.lazy_load()
-
-      -- load snippets from path/of/your/nvim/config/my-cool-snippets
-      loader.lazy_load({ paths = { "./snippets" } })
-
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      cmp.setup({
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol_text",
-            ellipsis_char = "...",
-            maxwidth = 50,
-            before = function(entry, vim_item)
-              vim_item = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
-              return vim_item
-            end,
-          }),
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = {
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-u>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({
-            select = true,
-          }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, {
-            "i",
-            "s",
-          }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, {
-            "i",
-            "s",
-          }),
-        },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "emoji" },
-        }),
-        experimental = {
-          -- native_menu = false,
-          -- ghost_text = true,
-        },
-        window = {
-          documentation = cmp.config.window.bordered(),
-          completion = cmp.config.window.bordered(),
-        },
-      })
-
-      -- only enable nvim_lsp in lua files
-      vim.cmd(
-        [[ autocmd FileType lua lua require'cmp'.setup.buffer { sources = { { name = 'buffer' },{ name = 'nvim_lua'},{name = "nvim_lsp"}},} ]]
-      )
-
-      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-    end,
+    signature = { enabled = true, window = { border = "rounded" } },
+    keymap = { preset = "default" },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = "mono",
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
   },
+  opts_extend = { "sources.default" },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,21 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "folke/neodev.nvim" },
+    dependencies = { "folke/neodev.nvim", "saghen/blink.cmp" },
     config = function()
       -- Setup neovim lua configuration
       require("neodev").setup()
       -- require all language server modules
-      require("language_servers.typescript")
-      require("language_servers.eslint")
-      require("language_servers.biome")
-      require("language_servers.vue")
-      require("language_servers.css")
-      require("language_servers.html")
-      require("language_servers.luals")
-      require("language_servers.python")
-      require("language_servers.json")
-      require("language_servers.tailwind")
-      require("language_servers.yaml")
-      require("language_servers.prisma")
-      require("language_servers.emmet")
-      require("language_servers.gql")
-      require("language_servers.rust")
-      require("language_servers.go")
-      require("language_servers.deno")
-      require("language_servers.astro")
-      require("language_servers.java")
-      require("language_servers.markdown")
-      require("language_servers.bash")
-      require("language_servers.php")
-      require("language_servers.htmx")
+      -- require("language_servers.python")
+      -- require("language_servers.yaml")
+      -- require("language_servers.gql")
+      -- require("language_servers.rust")
+      -- require("language_servers.go")
+      -- require("language_servers.deno")
+      -- require("language_servers.java")
+      -- require("language_servers.bash")
+      -- require("language_servers.php")
+      -- require("language_servers.htmx")
 
       -- rounded border on :LspInfo
       require("lspconfig.ui.windows").default_options.border = "rounded"
@@ -59,6 +46,159 @@ return {
 
       vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
         border = "rounded",
+      })
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local root_pattern = require("lspconfig").util.root_pattern
+      local lspconfig = require("lspconfig")
+
+      -- TS/JS
+      lspconfig.ts_ls.setup({
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        cmd = { "typescript-language-server", "--stdio" },
+        root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", "index.js", "app.js"),
+        capabilities = capabilities,
+      })
+
+      -- EsLint
+      require("lspconfig").eslint.setup({
+        root_dir = root_pattern(
+          -- "package.json",
+          ".eslintrc",
+          ".eslintrc.json",
+          ".eslintrc.yaml",
+          ".eslintrc.yml",
+          ".eslintrc.js",
+          ".eslintrc.cjs"
+        ),
+      })
+
+      -- Vue JS
+      require("lspconfig").volar.setup({})
+
+      -- Biome
+      require("lspconfig").biome.setup({})
+
+      -- CSS
+      local css_settings = {
+        validate = true,
+        lint = {},
+      }
+
+      local is_tailwind = vim.fn.filereadable(vim.fn.expand("tailwind.config.*"))
+      if is_tailwind == 1 then
+        css_settings.lint.unknownAtRules = "ignore"
+      end
+
+      require("lspconfig").cssls.setup({
+        capabilities = capabilities,
+        cmd = { "vscode-css-language-server", "--stdio" },
+        filetypes = { "css", "scss", "less" },
+        settings = {
+          css = css_settings,
+          less = {
+            validate = true,
+          },
+          scss = {
+            validate = true,
+          },
+        },
+      })
+      -- HTML
+      require("lspconfig").html.setup({
+        capabilities = capabilities,
+        cmd = { "vscode-html-language-server", "--stdio" },
+        filetypes = { "html", "php" },
+        init_options = {
+          configurationSection = { "html", "css", "javascript" },
+          embeddedLanguages = {
+            css = true,
+            javascript = true,
+          },
+        },
+      })
+
+      -- JSON
+      local uv = vim.loop
+
+      -- Function to check if biome.json exists in the current working directory
+      local function biome_file_exists()
+        local cwd = vim.fn.getcwd()
+        local biome_file = cwd .. "/biome.json"
+        return uv.fs_stat(biome_file) ~= nil
+      end
+
+      -- Conditionally set up jsonls if biome.json does not exist
+      if not biome_file_exists() then
+        lspconfig.jsonls.setup({
+          cmd = { "vscode-json-language-server", "--stdio" },
+          filetypes = { "json", "jsonc" },
+          init_options = {
+            provideFormatter = true,
+          },
+        })
+      else
+        print("biome.json found, not enabling jsonls")
+      end
+
+      -- Tailwind
+      require("lspconfig").tailwindcss.setup({
+        root_dir = root_pattern(
+          "tailwind.config.js",
+          "tailwind.config.ts",
+          "postcss.config.js",
+          "postcss.config.ts",
+          "tailwind.config.cjs"
+        ),
+      })
+
+      -- Emmet
+      require("lspconfig").emmet_ls.setup({
+        filetypes = { "html", "css", "javascriptreact", "typescriptreact", "vue", "php", "astro" },
+      })
+
+      -- Astro
+      require("lspconfig").astro.setup({})
+
+      -- Markdown
+      require("lspconfig").marksman.setup({})
+
+      -- Prisma
+      require("lspconfig").prismals.setup({
+        root_dir = function()
+          return vim.loop.cwd()
+        end,
+      })
+
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { "vim" },
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
       })
     end,
   },
