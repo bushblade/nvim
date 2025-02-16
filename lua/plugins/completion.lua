da03d058d3b480fbdd294fe8819630e9665aaa57
Will Adams <bushblade@gmail.com>
2025-02-16 20:23:13 +0000

    fix: breaking changes after update

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,7 +8,19 @@ return {
       documentation = { window = { border = "rounded" } },
     },
     signature = { enabled = true, window = { border = "rounded" } },
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
+      ["<CR>"] = { "select_and_accept", "fallback" },
+      ["<Tab>"] = { "select_next", "show_documentation", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "show_signature", "fallback" },
+      ["<C-l>"] = { "show_signature", "hide_signature", "fallback" },
+    },
+    -- cmdline = {
+    -- keymap = {
+    --   -- preset = "enter",
+    --   ["<CR>"] = { "fallback" },
+    -- },
+    -- },
     appearance = {
       use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
