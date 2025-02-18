c6e3248c59e9bdd7bb903e2880e5e6a63463c1ec
Will Adams <bushblade@gmail.com>
2025-02-18 16:12:44 +0000

    remove unused

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,9 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      flavour = "mocha",
-    },
     config = function()
       require("catppuccin").setup({
         flavour = "mocha", -- latte, frappe, macchiato, mocha
