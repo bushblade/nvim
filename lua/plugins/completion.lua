fbc90a0da127ad3969b71e832d0080dc4f69df02
Will Adams <bushblade@gmail.com>
2025-04-14 20:12:59 +0100

    load custom snippets too

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,6 +7,7 @@ return {
       dependencies = { "rafamadriz/friendly-snippets" },
       config = function()
         require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
       end,
     },
     "moyiz/blink-emoji.nvim",
