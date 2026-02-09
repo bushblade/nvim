8115684ac35b035e51fc91ffe5793d84abb65bd2
Will Adams <bushblade@gmail.com>
2026-02-09 16:02:33 +0000

    switch colorizer plugin

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,25 +1,8 @@
 return {
   {
-    "NvChad/nvim-colorizer.lua",
-    event = "VimEnter",
-    opts = {
-      filetypes = { "*" },
-      user_default_options = {
-        rgb_fn = true, -- CSS rgb() and rgba() functions
-        hsl_fn = true, -- CSS hsl() and hsla() functions
-        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = "background", -- Set the display mode.
-        -- Available methods are false / true / "normal" / "lsp" / "both"
-        -- True is same as normal
-        tailwind = true, -- Enable tailwind colors
-        -- parsers can contain values used in |user_default_options|
-        sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
-        virtualtext = "■",
-      },
-      -- all the sub-options of filetypes apply to buftypes
-      buftypes = {},
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
     },
   },
 }
