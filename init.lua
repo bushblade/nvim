a40fbb0ccfb69ae986b0740aee46a509a8b56a1c
will <bushblade@gmail.com>
2026-03-24 17:08:53 +0000

    update use of Lazy

--- init.lua
+++ init.lua
@@ -6,21 +6,33 @@ require("options")
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_opts = { ui = { border = "rounded" }, rocks = { enabled = false } }
+local lazy_opts = {
+  ui = { border = "rounded" },
+  rocks = { enabled = false },
+  spec = {
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+}
 
-require("lazy").setup("plugins", lazy_opts)
+require("lazy").setup(lazy_opts)
 
 -- keymappings
 require("keymappings")
